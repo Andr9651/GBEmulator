449e279220749cc90960727b4ba2499bf7991edf
Andreas <Holmi1@live.dk>
2025-02-06 14:38:21 +0100

    Implement instructions 0x40 to 0x7F except for 0x76 (HALT)

--- Emulator/CentalProcessingUnit/Instructions.cs
+++ Emulator/CentalProcessingUnit/Instructions.cs
@@ -41,6 +41,16 @@ public partial class CPU
         return value;
     }
 
+    private byte LoadFromAddressInHL()
+    {
+        return _mmu.Read8(HL);
+    }
+
+    private void WriteToAddressInHL(byte value)
+    {
+        _mmu.Write8(HL, value);
+    }
+
     private void ExecuteInstruction(byte instructionCode)
     {
         // ProgramCounter increments have been deferred so this takes care of the increment that would have happened when reading the current instruction
@@ -207,135 +217,199 @@ public partial class CPU
                 break;
 
             case 0x40:
+                B = B; // This instruction seems useless
                 break;
             case 0x41:
+                B = C;
                 break;
             case 0x42:
+                B = D;
                 break;
             case 0x43:
+                B = E;
                 break;
             case 0x44:
+                B = H;
                 break;
             case 0x45:
+                B = L;
                 break;
             case 0x46:
+                B = LoadFromAddressInHL();
                 break;
             case 0x47:
+                B = Accumulator;
                 break;
             case 0x48:
+                C = B;
                 break;
             case 0x49:
+                C = C; // This instruction seems useless
                 break;
             case 0x4A:
+                C = D;
                 break;
             case 0x4B:
+                C = E;
                 break;
             case 0x4C:
+                C = H;
                 break;
             case 0x4D:
+                C = L;
                 break;
             case 0x4E:
+                C = LoadFromAddressInHL();
                 break;
             case 0x4F:
+                C = Accumulator;
                 break;
 
             case 0x50:
+                D = B;
                 break;
             case 0x51:
+                D = C;
                 break;
             case 0x52:
+                D = D; // This instruction seems useless
                 break;
             case 0x53:
+                D = E;
                 break;
             case 0x54:
+                D = H;
                 break;
             case 0x55:
+                D = L;
                 break;
             case 0x56:
+                D = LoadFromAddressInHL();
                 break;
             case 0x57:
+                D = Accumulator;
                 break;
             case 0x58:
+                E = B;
                 break;
             case 0x59:
+                E = C;
                 break;
             case 0x5A:
+                E = D;
                 break;
             case 0x5B:
+                E = E; // This instruction seems useless
                 break;
             case 0x5C:
+                E = H;
                 break;
             case 0x5D:
+                E = L;
                 break;
             case 0x5E:
+                E = LoadFromAddressInHL();
                 break;
             case 0x5F:
+                E = Accumulator;
                 break;
 
             case 0x60:
+                H = B;
                 break;
             case 0x61:
+                H = C;
                 break;
             case 0x62:
+                H = D;
                 break;
             case 0x63:
+                H = E;
                 break;
             case 0x64:
+                H = H; // This instruction seems useless
                 break;
             case 0x65:
+                H = L;
                 break;
             case 0x66:
+                H = LoadFromAddressInHL();
                 break;
             case 0x67:
+                H = Accumulator;
                 break;
             case 0x68:
+                L = B;
                 break;
             case 0x69:
+                L = C;
                 break;
             case 0x6A:
+                L = D;
                 break;
             case 0x6B:
+                L = E;
                 break;
             case 0x6C:
+                L = H;
                 break;
             case 0x6D:
+                L = L; // This instruction seems useless
                 break;
             case 0x6E:
+                L = LoadFromAddressInHL();
                 break;
             case 0x6F:
+                L = Accumulator;
                 break;
 
             case 0x70:
+                WriteToAddressInHL(B);
                 break;
             case 0x71:
+                WriteToAddressInHL(C);
                 break;
             case 0x72:
+                WriteToAddressInHL(D);
                 break;
             case 0x73:
+                WriteToAddressInHL(E);
                 break;
             case 0x74:
+                WriteToAddressInHL(H);
                 break;
             case 0x75:
+                WriteToAddressInHL(L);
                 break;
             case 0x76:
+                throw new NotImplementedException("HALT not implemented");
                 break;
             case 0x77:
+                WriteToAddressInHL(Accumulator);
                 break;
             case 0x78:
+                Accumulator = B;
                 break;
             case 0x79:
+                Accumulator = C;
                 break;
             case 0x7A:
+                Accumulator = D;
                 break;
             case 0x7B:
+                Accumulator = E;
                 break;
             case 0x7C:
+                Accumulator = H;
                 break;
             case 0x7D:
+                Accumulator = L;
                 break;
             case 0x7E:
+                Accumulator = LoadFromAddressInHL();
                 break;
             case 0x7F:
+                Accumulator = Accumulator; // This instruction seems useless
                 break;
 
             case 0x80:
