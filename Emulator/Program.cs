782b18defea247cf0be3e804dd419dfc17bae53b
Andreas <Holmi1@live.dk>
2025-02-08 22:08:33 +0100

    Add 0xCB instruction switch statement

--- Emulator/Program.cs
+++ Emulator/Program.cs
@@ -5,6 +5,10 @@ using GBemulator.MemoryManagementUnit;
 // {
 //     Console.WriteLine($"case 0x{i:X2}: ");
 //     Console.WriteLine("break;");
+//     if (i % 8 == 7)
+//     {
+//         Console.WriteLine();
+//     }
 // }
 
 byte[] program = {
