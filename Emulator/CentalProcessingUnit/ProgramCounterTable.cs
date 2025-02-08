1e0c0cac57948e5c19fca23f06826f0347109d83
Andreas <Holmi1@live.dk>
2025-02-08 19:40:11 +0100

    Update program counter increments table to include 0xCB

--- Emulator/CentalProcessingUnit/ProgramCounterTable.cs
+++ Emulator/CentalProcessingUnit/ProgramCounterTable.cs
@@ -4,6 +4,7 @@ namespace GBemulator.CentralProcessingUnit;
 public partial class CPU
 {
     // Data copied from https://meganesu.github.io/generate-gb-opcodes/
+    // 0xCB set to 2 instead of 0 to account for 16-bit instructions
     private readonly byte[] ProgramCounterIncrementsPerInstruction = {
     //  x0	x1	x2	x3	x4	x5	x6	x7	x8	x9	xA	xB	xC	xD	xE	xF
         1,  3,  1,  1,  1,  1,  2,  1,  3,  1,  1,  1,  1,  1,  2,  1, // 0x
@@ -18,7 +19,7 @@ public partial class CPU
         1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, // 9x
         1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, // Ax
         1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, // Bx
-        1,  1,  3,  3,  3,  1,  2,  1,  1,  1,  3,  0,  3,  3,  2,  1, // Cx
+        1,  1,  3,  3,  3,  1,  2,  1,  1,  1,  3,  2,  3,  3,  2,  1, // Cx
         1,  1,  3,  0,  3,  1,  2,  1,  1,  1,  3,  0,  3,  0,  2,  1, // Dx
         2,  1,  1,  0,  0,  1,  2,  1,  2,  1,  3,  0,  0,  0,  2,  1, // Ex
         2,  1,  1,  1,  0,  1,  2,  1,  2,  1,  3,  1,  0,  0,  2,  1, // Fx
@@ -27,10 +28,5 @@ public partial class CPU
     private void IncrementProgramCounter(ushort instructionCode)
     {
         ProgramCounter += ProgramCounterIncrementsPerInstruction[instructionCode];
-
-        if (instructionCode == 0xCB)
-        {
-            ProgramCounter += 2;
-        }
     }
 }
