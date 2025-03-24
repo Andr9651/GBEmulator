8ac09095f9e4fd9f93e34670529878b04bf0d43c
Andreas <Holmi1@live.dk>
2025-03-24 13:08:27 +0100

    Complete first draft of timer handling and the timer interrupt Also replace write8 with write8Mapped

--- Emulator/MemoryManagementUnit/MMU.cs
+++ Emulator/MemoryManagementUnit/MMU.cs
@@ -9,10 +9,14 @@ public class MMU
     private readonly byte[] _bootROM;
     private bool _bootMode;
 
+    // T-cycles
+    private ushort _divider;
+
+    // Only the upper 8 bits of the _divider is exposed through the MMU
     private byte _dividerRegister
     {
-        get => _memory[0xFF04];
-        set => _memory[0xFF04] = value;
+        get => (byte)(_divider >> 8);
+        set => _divider = 0;
     }
 
     private byte _timerCounter
@@ -35,20 +39,20 @@ public class MMU
 
     private bool _timerControlEnable
     {
-        get => _memory[0xFF07].GetBit(2);
-        set => _memory[0xFF07] = _memory[0xFF07].SetBit(2, value);
+        get => _timerControl.GetBit(2);
+        set => _timerControl = _timerControl.SetBit(2, value);
     }
 
+    // Increment interval of selected clock in T-cycles
     private int _timerControlClockSelect
     {
         get => (_memory[0xFF07] & 0b0000_0011) switch
         {
-            0b0000_0000 => 256,
-            0b0000_0001 => 4,
-            0b0000_0010 => 16,
-            0b0000_0011 => 64,
-
-            _ => 256,
+            0b0000_0000 => 1024,
+            0b0000_0001 => 16,
+            0b0000_0010 => 64,
+            0b0000_0011 => 256,
+            _ => 1024,
         };
     }
 
@@ -96,32 +100,31 @@ public class MMU
         <= 0xFDFF => Read8Direct((ushort)(address - 0x2000)),       // Echo RAM (mirror of C000–DDFF)
         <= 0xFE9F => Read8Direct(address),                          // Object attribute memory (OAM)
         <= 0xFEFF => 0xFF,                                          // Not Usable, see https://gbdev.io/pandocs/Memory_Map.html#fea0feff-range
+        0xFF04 => _dividerRegister,
         0xFF44 => 0x90,                                             // Hardcoded LCD for Gameboy Doctor
         <= 0xFF7F => Read8Direct(address),                          // I/O Registers
         <= 0xFFFE => Read8Direct(address),                          // High RAM
         <= 0xFFFF => Read8Direct(address),                          // Interrupt Enable Register (IE)
     };
 
-    public void Write8Mapped(ushort address, byte value)
+    public void Write8(ushort address, byte value)
     {
         switch (address)
         {
             case <= 0x3FFF: break;                                              // ROM Bank 00
             case <= 0x7FFF: break;                                              // ROM Bank 01-NN (switchable ROM Bank)
-            case <= 0x9FFF: Write8(address, value); break;                      // VRAM (Switchable in CGB mode)
-            case <= 0xBFFF: Write8(address, value); break;                      // External RAM
-            case <= 0xCFFF: Write8(address, value); break;                      // Work RAM
-            case <= 0xDFFF: Write8(address, value); break;                      // Work RAM (Switchable in CGB mode)
-            case <= 0xFDFF: Write8((ushort)(address - 0x2000), value); break;   // Echo RAM (mirror of C000–DDFF)
-            case <= 0xFE9F: Write8(address, value); break;                      // Object attribute memory (OAM)
+            case <= 0x9FFF: Write8Direct(address, value); break;                      // VRAM (Switchable in CGB mode)
+            case <= 0xBFFF: Write8Direct(address, value); break;                      // External RAM
+            case <= 0xCFFF: Write8Direct(address, value); break;                      // Work RAM
+            case <= 0xDFFF: Write8Direct(address, value); break;                      // Work RAM (Switchable in CGB mode)
+            case <= 0xFDFF: Write8Direct((ushort)(address - 0x2000), value); break;   // Echo RAM (mirror of C000–DDFF)
+            case <= 0xFE9F: Write8Direct(address, value); break;                      // Object attribute memory (OAM)
             case <= 0xFEFF: break;                                              // Not Usable, see https://gbdev.io/pandocs/Memory_Map.html#fea0feff-range
-            case 0xFF04: Write8(address, 0x00); break;                          // DIB: Divider register  
-            case 0xFF05: Write8(address, 0x00); break;                          // TIMA: Timer counter  
-            case 0xFF06: Write8(address, 0x00); break;                          // TMA: Timer modulo  
-            case 0xFF07: Write8(address, 0x00); break;                          // TCA: Timer control  
-            case <= 0xFF7F: Write8(address, value); break;                      // I/O Registers
-            case <= 0xFFFE: Write8(address, value); break;                      // High RAM
-            case <= 0xFFFF: Write8(address, value); break;                      // Interrupt Enable Register (IE)
+            case 0xFF04: _dividerRegister = 0; break;                          // DIB: Divider register  
+            case 0xFF07: LogWrite8Direct(address, value, "TAC: "); break;                          // TCA: Timer control  
+            case <= 0xFF7F: Write8Direct(address, value); break;                      // I/O Registers
+            case <= 0xFFFE: Write8Direct(address, value); break;                      // High RAM
+            case <= 0xFFFF: LogWrite8Direct(address, value, "Interrupt enable: "); break;                      // Interrupt Enable Register (IE)
         }
     }
 
@@ -132,25 +135,26 @@ public class MMU
 
     public ushort Read16(ushort address)
     {
-        byte leastSignificant = _memory[address];
-        byte mostSignificant = _memory[(ushort)(address + 1)];
+        byte leastSignificant = Read8(address);
+        byte mostSignificant = Read8((ushort)(address + 1));
 
         ushort result = Helpers.BytesToUshort(mostSignificant, leastSignificant);
 
         return result;
     }
+    private void LogWrite8Direct(ushort address, byte value, string? name = null)
+    {
+        Console.WriteLine($"{(name != null ? name : address)}{value:b8}");
+        Write8Direct(address, value);
+    }
 
-    public void Write8(ushort address, byte value)
+    public void Write8Direct(ushort address, byte value)
     {
         if (address == 0xFF01) // Serial buffer address
         {
             Console.Write((char)value);
         }
 
-        if (address == 0xFFFF) // Serial buffer address
-        {
-            Console.WriteLine($"Interrupt Enable: {value:b8}");
-        }
         _memory[address] = value;
     }
 
@@ -158,8 +162,8 @@ public class MMU
     {
         var (mostSignificant, leastSignificant) = Helpers.UshortToBytes(value);
 
-        _memory[address] = leastSignificant;
-        _memory[(ushort)(address + 1)] = mostSignificant;
+        Write8(address, leastSignificant);
+        Write8((ushort)(address + 1), mostSignificant);
     }
 
     public Interrupt GetNextInterrupt()
@@ -192,17 +196,25 @@ public class MMU
     }
 
     // https://gbdev.io/pandocs/Timer_and_Divider_Registers.html
-    public void IncrementTimersByMCycles(int value)
+    public void IncrementTimersByMCycles(int mCycles)
     {
+        int newDivider = _divider + mCycles * 4;
 
-        int newDivider = _dividerRegister + value * 4;
+        int oldClockTicks = _divider / _timerControlClockSelect;
+        int newClockTicks = newDivider / _timerControlClockSelect;
+        int clockTicks = newClockTicks - oldClockTicks;
 
-        int newCounter = _timerCounter + value * 4;
-
-        if (newCounter > 0xFF)
+        if (_timerControlEnable == true && clockTicks > 0)
         {
-            _timerCounter = (byte)(_timerModulo + (newCounter - 0xFF));
-            _interruptFlags = _interruptFlags.SetBit(2, true);
+            int newTimerCounter = _timerCounter + clockTicks;
+            _timerCounter = (byte)newTimerCounter;
+            if (newTimerCounter > 0xFF)
+            {
+                _timerCounter = (byte)(_timerModulo + (newTimerCounter - 0xFF));
+                _interruptFlags = _interruptFlags.SetBit(2, true);
+            }
         }
+
+        _divider = (ushort)newDivider;
     }
 }
